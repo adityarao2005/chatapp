4b0da82581cf01b33d1088cd04d9e5f611c561fd
Aditya Rao <adityarao2005@gmail.com>
2024-07-15 14:21:43 -0400

    starting from the beginnning again

--- frontend-react/src/components/chat-panel.tsx
+++
@@ -1,6 +0,0 @@
-
-export default function ChatPanel() {
-	return (
-		<div className="basis-2/3 border-2 border-black"></div>
-	);
-}
--- frontend-react/src/components/friends-panel.tsx
+++
@@ -1,6 +0,0 @@
-
-export default function FriendsPanel() {
-	return (
-		<div className="basis-1/3 border-2 border-black"></div>
-	);
-}
